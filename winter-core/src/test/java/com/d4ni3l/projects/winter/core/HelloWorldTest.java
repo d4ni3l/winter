874dac7fde0367f1578ad208e8f7de3fee5e19b9
Dan7 <harryandbellatrix@gmail.com>
2017-12-19 13:25:32 +0330

    Adding TestNG suite

---
+++ winter-core/src/test/java/com/d4ni3l/projects/winter/core/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package com.d4ni3l.projects.winter.core;
+
+import static org.testng.Assert.*;
+
+public class HelloWorldTest {
+
+    @org.testng.annotations.Test
+    public void testMain() throws Exception {
+    }
+}
