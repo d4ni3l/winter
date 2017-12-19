94cd72898a35d644bd0da878e00c7a4a8c30c7f2
Dan7 <harryandbellatrix@gmail.com>
2017-12-19 13:27:45 +0330

    Replaced the qualified annotation with an import

--- winter-core/src/test/java/com/d4ni3l/projects/winter/core/HelloWorldTest.java
+++ winter-core/src/test/java/com/d4ni3l/projects/winter/core/HelloWorldTest.java
@@ -1,10 +1,12 @@
 package com.d4ni3l.projects.winter.core;
 
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class HelloWorldTest {
 
-    @org.testng.annotations.Test
+    @Test
     public void testMain() throws Exception {
     }
 }
